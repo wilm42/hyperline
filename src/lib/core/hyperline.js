88c803759e75ad6b24cac97d47195c3732f18fc4
wilm <william@twobit.cc>
2017-10-05 14:50:12 -0400

    move hyperline to top right of term so it plays nice with statusline plugin

--- src/lib/core/hyperline.js
+++ src/lib/core/hyperline.js
@@ -1,52 +1,53 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Component from 'hyper/component'
-import decorate from 'hyper/decorate'
+import React from "react";
+import PropTypes from "prop-types";
+import Component from "hyper/component";
+import decorate from "hyper/decorate";
 
 class HyperLine extends Component {
-  static propTypes() {
-    return {
-      plugins: PropTypes.array.isRequired
-    }
-  }
+	static propTypes() {
+		return {
+			plugins: PropTypes.array.isRequired,
+		};
+	}
 
-  styles() {
-    return {
-      line: {
-        display: 'flex',
-        alignItems: 'center',
-        position: 'absolute',
-        overflow: 'hidden',
-        bottom: '-1px',
-        width: '100%',
-        height: '18px',
-        font: 'bold 10px Monospace',
-        pointerEvents: 'none',
-        background: 'rgba(0, 0, 0, 0.08)',
-        margin: '10px 10px 0 10px'
-      },
-      wrapper: {
-        display: 'flex',
-        flexShrink: '0',
-        alignItems: 'center',
-        paddingLeft: '10px',
-        paddingRight: '10px'
-      }
-    }
-  }
-  template(css) {
-    const { plugins } = this.props
+	styles() {
+		return {
+			line: {
+				opacity: 0.3,
+				display: "flex",
+				alignItems: "center",
+				position: "absolute",
+				overflow: "hidden",
+				top: "0",
+				right: "0",
+				height: "18px",
+				width: "28%",
+				font: "10px Monospace",
+				pointerEvents: "none",
+				background: "rgba(0, 0, 0, 1)",
+				margin: "10px 10px 0 10px",
+			},
+			wrapper: {
+				display: "flex",
+				flexShrink: "0",
+				alignItems: "center",
+				padding: "10px",
+			},
+		};
+	}
+	template(css) {
+		const { plugins } = this.props;
 
-    return (
-      <div className={css('line')} {...this.props}>
-        {plugins.map((Component, index) => (
-          <div key={index} className={css('wrapper')}>
-            <Component />
-          </div>
-        ))}
-      </div>
-    )
-  }
+		return (
+			<div className={css("line")} {...this.props}>
+				{plugins.map((Component, index) => (
+					<div key={index} className={css("wrapper")}>
+						<Component />
+					</div>
+				))}
+			</div>
+		);
+	}
 }
 
-export default decorate(HyperLine, 'HyperLine')
+export default decorate(HyperLine, "HyperLine");
